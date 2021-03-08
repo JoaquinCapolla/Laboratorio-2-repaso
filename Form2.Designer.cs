db13a5829be58543166bf34eddf32f653edc8838
JoaquinCapilla <joaquincapilla@mesoamericana.edu.gt>
2021-03-08 08:49:48 -0600

    elaboracion de la segunda interfaz

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -29,12 +29,249 @@ namespace Laboratorio_2_repaso
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_marca = new System.Windows.Forms.Label();
+            this.lbl_modelo = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_nit = new System.Windows.Forms.TextBox();
+            this.txt_direccion = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_alquiler = new System.Windows.Forms.TextBox();
+            this.txt_devolucion = new System.Windows.Forms.TextBox();
+            this.txt_recorridos = new System.Windows.Forms.TextBox();
+            this.btn_agregar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(131, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Vehiculos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(322, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Marca";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(397, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Modelo";
+            // 
+            // lbl_marca
+            // 
+            this.lbl_marca.AutoSize = true;
+            this.lbl_marca.Location = new System.Drawing.Point(322, 30);
+            this.lbl_marca.Name = "lbl_marca";
+            this.lbl_marca.Size = new System.Drawing.Size(47, 13);
+            this.lbl_marca.TabIndex = 4;
+            this.lbl_marca.Text = "**********";
+            // 
+            // lbl_modelo
+            // 
+            this.lbl_modelo.AutoSize = true;
+            this.lbl_modelo.Location = new System.Drawing.Point(397, 30);
+            this.lbl_modelo.Name = "lbl_modelo";
+            this.lbl_modelo.Size = new System.Drawing.Size(47, 13);
+            this.lbl_modelo.TabIndex = 5;
+            this.lbl_modelo.Text = "**********";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nombre de cliente";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Nit de cliente";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Direc. de cliente";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(131, 49);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(238, 20);
+            this.txt_nombre.TabIndex = 9;
+            // 
+            // txt_nit
+            // 
+            this.txt_nit.Location = new System.Drawing.Point(131, 75);
+            this.txt_nit.Name = "txt_nit";
+            this.txt_nit.Size = new System.Drawing.Size(238, 20);
+            this.txt_nit.TabIndex = 10;
+            // 
+            // txt_direccion
+            // 
+            this.txt_direccion.Location = new System.Drawing.Point(131, 101);
+            this.txt_direccion.Name = "txt_direccion";
+            this.txt_direccion.Size = new System.Drawing.Size(238, 20);
+            this.txt_direccion.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(37, 130);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Fecha de alquiler";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(18, 156);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Fecha de devolucion";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(54, 182);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Km recorridos";
+            // 
+            // txt_alquiler
+            // 
+            this.txt_alquiler.Location = new System.Drawing.Point(131, 127);
+            this.txt_alquiler.Name = "txt_alquiler";
+            this.txt_alquiler.Size = new System.Drawing.Size(238, 20);
+            this.txt_alquiler.TabIndex = 15;
+            // 
+            // txt_devolucion
+            // 
+            this.txt_devolucion.Location = new System.Drawing.Point(131, 153);
+            this.txt_devolucion.Name = "txt_devolucion";
+            this.txt_devolucion.Size = new System.Drawing.Size(238, 20);
+            this.txt_devolucion.TabIndex = 16;
+            // 
+            // txt_recorridos
+            // 
+            this.txt_recorridos.Location = new System.Drawing.Point(131, 179);
+            this.txt_recorridos.Name = "txt_recorridos";
+            this.txt_recorridos.Size = new System.Drawing.Size(238, 20);
+            this.txt_recorridos.TabIndex = 17;
+            // 
+            // btn_agregar
+            // 
+            this.btn_agregar.Location = new System.Drawing.Point(375, 177);
+            this.btn_agregar.Name = "btn_agregar";
+            this.btn_agregar.Size = new System.Drawing.Size(75, 23);
+            this.btn_agregar.TabIndex = 18;
+            this.btn_agregar.Text = "Agregar";
+            this.btn_agregar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 214);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 224);
+            this.dataGridView1.TabIndex = 19;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_agregar);
+            this.Controls.Add(this.txt_recorridos);
+            this.Controls.Add(this.txt_devolucion);
+            this.Controls.Add(this.txt_alquiler);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_direccion);
+            this.Controls.Add(this.txt_nit);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl_modelo);
+            this.Controls.Add(this.lbl_marca);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form2";
             this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_marca;
+        private System.Windows.Forms.Label lbl_modelo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_nit;
+        private System.Windows.Forms.TextBox txt_direccion;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt_alquiler;
+        private System.Windows.Forms.TextBox txt_devolucion;
+        private System.Windows.Forms.TextBox txt_recorridos;
+        private System.Windows.Forms.Button btn_agregar;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
